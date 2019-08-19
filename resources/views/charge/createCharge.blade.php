06c31ceae681cf9472d21e30610163e9b2c7afb4
BryanHenao <bryanhenao09@gmail.com>
2019-08-19 18:43:19 -0500

    options adviser

--- resources/views/charge/createCharge.blade.php
+++ resources/views/charge/createCharge.blade.php
@@ -131,8 +131,5 @@
 
         </body>
 
-        <script src="{{ asset("asset/js/jquery-3.2.1.min.js") }}"></script>
-        <script src="{{ asset("asset/js/scriptApp.js") }}"></script>
-
     </html> 
     @endsection
