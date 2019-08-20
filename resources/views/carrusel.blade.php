63aa280666dc07c7ed53096bb629306c1883560b
programador2684 <programador2684@gmail.com>
2019-08-19 22:22:32 -0500

    carrusel

--- resources/views/carrusel.blade.php
+++ resources/views/carrusel.blade.php
@@ -2,35 +2,45 @@
 
 @section('content')
 
-<div id="myCarousel" class="carousel slide" data-ride="carousel">
+<section class="awSlider">
+  <div  class="carousel slide" data-ride="carousel">
     <!-- Indicators -->
     <ol class="carousel-indicators">
-        <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-        <li data-target="#myCarousel" data-slide-to="1"></li>
-        <li data-target="#myCarousel" data-slide-to="2"></li>
+      <li data-target=".carousel" data-slide-to="0" class="active"></li>
+      <li data-target=".carousel" data-slide-to="1"></li>
+      <li data-target=".carousel" data-slide-to="2"></li>
+      <li data-target=".carousel" data-slide-to="3"></li>
     </ol>
 
     <!-- Wrapper for slides -->
-    <div class="carousel-inner">
-        <div class="item active">
-            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTwwQRAUC08y3aqZ9QnpJjdRX1cpeBM3H-uw6Z9okwsfpCWv0rYRA" style="width:100% !important; height:250px !important;"> 
-        </div>
-        <div class="item">
-            <img src="images/image-2.jpeg" >
-        </div>
-        <div class="item">
-            <img src="images/image-3.jpeg" width="100%">
-        </div>
+    <div class="carousel-inner" role="listbox">
+      <div class="item active">
+        <img src="http://www.adobewordpress.com/wp-content/uploads/2014/02/wallpaper-thumb-941.jpg">
+        <div class="carousel-caption">Görsel #1</div>
+      </div>
+      <div class="item">
+        <img src="http://www.adobewordpress.com/wp-content/uploads/2014/02/wallpaper-thumb-101.jpg">
+        <div class="carousel-caption">Görsel #2</div>
+      </div>
+      <div class="item">
+        <img src="http://www.adobewordpress.com/wp-content/uploads/2014/02/wallpaper-thumb-1051.jpg">
+        <div class="carousel-caption">Görsel #3</div>
+      </div>
+      <div class="item">
+        <img src="http://www.adobewordpress.com/wp-content/uploads/2013/07/wallpaper-thumb-74.jpg">
+        <div class="carousel-caption">Görsel #4</div>
+      </div>
     </div>
 
     <!-- Controls -->
-    <a class="left carousel-control" href="#myCarousel" data-slide="prev">
-        <span class="glyphicon glyphicon-chevron-left"></span>
-        <span class="sr-only">Previous</span>
+    <a class="left carousel-control" href=".carousel" role="button" data-slide="prev">
+      <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+      <span class="sr-only">Geri</span>
     </a>
-    <a class="right carousel-control" href="#myCarousel" data-slide="next">
-        <span class="glyphicon glyphicon-chevron-right"></span>
-        <span class="sr-only">Next</span>
+    <a class="right carousel-control" href=".carousel" role="button" data-slide="next">
+      <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+      <span class="sr-only">İleri</span>
     </a>
-</div>
+  </div>
+</section>
 @endsection
